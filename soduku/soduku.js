e7e432a5ed34f2036480236c4f30fd17cbd80ccb
Caleb Marchent <calebmarchent@Calebs-MacBook-Pro.local>
2015-12-26 22:37:32 +0100

    Can now represent eliminated possibilites in a box

--- soduku/soduku.js
+++ soduku/soduku.js
@@ -3,28 +3,49 @@ var d = 0;
 
 function sodukuBox(value)
 {
-   if (value)
-   {
-      return document.createTextNode(value);
-   }
-   else
-   {
-   var ntbl = document.createElement('table');
-   var ntbdy = document.createElement('tbody');
-   ntbl.className += 'box';
-   var c = 1;
-   for (var k = 0; k < 3; k++) {
-       var ntr = document.createElement('tr');
-       for (var l = 0; l < 3; l++) {
-           var ntd = document.createElement('td');
-           ntd.appendChild(document.createTextNode(c++));
-           ntr.appendChild(ntd);
+    var bx = document.createElement("div");
+    bx.possibility = [];
+    for (var c = 1; c <= 9; c++)
+    {
+         bx.possibility[c] = document.createElement('td');
+         bx.possibility[c].possible = true;
+         bx.possibility[c].appendChild(document.createTextNode(c));
+    }
+
+    if (value)
+    {
+        bx.appendChild(document.createTextNode(value));
+    }
+    else
+    {
+        tbl = document.createElement('table');
+        var ntbdy = document.createElement('tbody');
+        tbl.className += 'box';
+        for (var k = 0; k < 3; k++) 
+        {
+            var ntr = document.createElement('tr');
+            for (var l = 0; l < 3; l++) 
+            {
+                var c = 1 + (3*k) + l;
+                ntr.appendChild(bx.possibility[c]);
+            }
+            ntbdy.appendChild(ntr);
+        }
+        tbl.appendChild(ntbdy);
+        bx.appendChild(tbl);
+    }
+
+    bx.refresh = function ()
+    {
+       for (var c = 1; c <= 9; c++)
+       {
+          this.possibility[c].className = this.possibility[c].possible ? 'isposs' : 'notposs' ;
        }
-       ntbdy.appendChild(ntr);
-   }
-   ntbl.appendChild(ntbdy);
-   return ntbl;
-   }
+    }
+    bx.possibility[5].possible = false; 
+
+    bx.refresh();
+    return bx;
 }
 
 
@@ -40,22 +61,22 @@ function createSodukuTable()
         for (var j = 0; j < 3; j++) 
         {
             var td = document.createElement('td');
-             var ntbl = document.createElement('table');
-             var ntbdy = document.createElement('tbody');
-             ntbl.className += 'square';
-             for (var m = 0; m < 3; m++)
-              {
-                 var ntr = document.createElement('tr');
-                 for (var n = 0; n < 3; n++)
-                 {
+            var ntbl = document.createElement('table');
+            var ntbdy = document.createElement('tbody');
+            ntbl.className += 'square';
+            for (var m = 0; m < 3; m++)
+            {
+                var ntr = document.createElement('tr');
+                for (var n = 0; n < 3; n++)
+                {
                     var ntd = document.createElement('td');
                     var x = (i * 3) + m;
                     var y = (j * 3) + n;
                     var sb = sodukuBox(puzzle[x][y]);
                     ntd.appendChild(sb);
                     ntr.appendChild(ntd);
-                 }
-                 ntbdy.appendChild(ntr);
+                }
+                ntbdy.appendChild(ntr);
             }
             ntbl.appendChild(ntbdy);
             td.appendChild(ntbl);
