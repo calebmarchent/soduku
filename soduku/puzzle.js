8ecc6c3bb55ff75f0593813939089cc8bef20c34
Caleb Marchent <caleb.marchent@iee.org>
2015-12-28 17:23:16 +0100

    Remove explicit nulls from puzzle array.
    The code appears to work for Chrome without them, and it makes it easier
    to copy a puzzle into.

--- soduku/puzzle.js
+++ soduku/puzzle.js
@@ -1,13 +1,13 @@
 
 
 var puzzle = [
-    [    8,    3, null, null, null, null, null, null, null ],
-    [ null, null,    1, null,    8, null,    2, null,    9 ],
-    [ null,    2,    4, null,    1,    7, null, null, null ],
-    [ null,    9,    2, null,    5,    3, null, null,    6 ],
-    [ null, null,    5, null,    9, null,    8, null, null ],
-    [    3, null, null,    4,    6, null,    5,    9, null ],
-    [ null, null, null,    5,    7, null,    3,    6, null ],
-    [    7, null,    3, null,    4, null,    9, null, null ],
-    [ null, null, null, null, null, null, null,    7,    8 ]
+  [  8,  3,   ,   ,   ,   ,   ,   ,    ],
+  [   ,   ,  1,   ,  8,   ,  2,   ,  9 ],
+  [   ,  2,  4,   ,  1,  7,   ,   ,    ],
+  [   ,  9,  2,   ,  5,  3,   ,   ,  6 ],
+  [   ,   ,  5,   ,  9,   ,  8,   ,    ],
+  [  3,   ,   ,  4,  6,   ,  5,  9,    ],
+  [   ,   ,   ,  5,  7,   ,  3,  6,    ],
+  [  7,   ,  3,   ,  4,   ,  9,   ,    ],
+  [   ,   ,   ,   ,   ,   ,   ,  7,  8 ]
  ]; 
